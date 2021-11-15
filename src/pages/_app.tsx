d7ad295e95ff6fb99d004942d9cb8a32be47f87b
Kuznetsov Eric <kuznetsov.eric@gmail.com>
2021-11-15 15:26:22 +0300

    GA added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
-import { memo, FC } from "react";
+import { memo, FC, useEffect } from "react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import * as gtag from "@/lib/ga";
 
 import "@/styles/index.css";
 
 const MyApp: FC<AppProps> = memo(({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 });
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,28 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {/* eslint-disable react/no-danger, @typescript-eslint/no-non-null-assertion */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env
+              .NEXT_PUBLIC_GOOGLE_ANALYTICS!}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS!}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          {/* eslint-enable */}
+        </Head>
         <body>
           <Main />
           <NextScript />
